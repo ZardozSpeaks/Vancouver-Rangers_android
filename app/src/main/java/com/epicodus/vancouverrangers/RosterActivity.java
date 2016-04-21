551b886007a4490319b432c7fff20d29f09dd397
david remington and kevin mattison <david.ian.remington@gmail.com>
2016-04-21 16:02:56 -0700

    Remove list view roster and start on tabbed view roster.

--- app/src/main/java/com/epicodus/vancouverrangers/RosterActivity.java
+++ app/src/main/java/com/epicodus/vancouverrangers/RosterActivity.java
@@ -1,47 +1,159 @@
 package com.epicodus.vancouverrangers;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
+
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class RosterActivity extends AppCompatActivity {
-    //private TextView mGoaliesTextView;
-    private ListView mRosterListView;
 
-    private String [] roster = new String[] {"#1 - Jamie Nanchen - Goalie", "#20 - Aaron Daniello - Goalie", "#30 - Liam Bohm-Meyer - Goalie",
-            "#33 - Brenden Leise - Goalie", "#5 - Bryce Ebert - Defense", "#7 - Matteo Toneatto - Defense", "#9 - Michael Hennekens - Defense",
-            "#15 - Chris Alberts - Defense", "#16 - Jacob Rushlow - Defense", "#24 - (A)Billy Wildman - Defense", "#96 - Keegan Kessler - Defense", "#97 - Travis Bobb - Defense",
-            "#8 - Martin Cierny - Forward", "#10 - Kyle Stevens - Forward", "#12 - Brandon Aab - Forward", "#13 - Joel Lehtonen - Forward",
-            "#14 - Paul Frys - Forward", "#17 - Jesse Vuorinen - Forward", "#18 - Grayson Szumilas - Forward","#21 - (C)Spence Cowand - Forward", "#42 - Ayrton Chapman - Forward",
-            "#54 - Josh Davenport - Forward", "#88 - Vladislav Polyashov - Forward", "#89 - Simon Uppman - Forward", "#91 - Tuomas Korhonen - Forward", "#95 - Elijah Bordak - Forward",
-            "#98 - Dustin Johnson - Forward"};
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_roster);
+        setContentView(R.layout.activity_roster3);
 
-        mRosterListView = (ListView) findViewById(R.id.rosterListView);
-        ArrayAdapter goalieAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, roster);
-        mRosterListView.setAdapter(goalieAdapter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        mRosterListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String roster = ((TextView)view).getText().toString();
-                Toast.makeText(RosterActivity.this, roster, Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
+    }
 
 
-        //mGoaliesTextView = (TextView) findViewById(R.id.goaliesTextView);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_roster, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_roster, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
     }
 }
