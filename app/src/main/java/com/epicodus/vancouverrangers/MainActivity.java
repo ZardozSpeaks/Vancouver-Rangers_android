d28d795cfd2b935a3e2c2fe102c9b9e8c66abe5c
david remington and kevin mattison <david.ian.remington@gmail.com>
2016-04-21 09:50:56 -0700

    Bind all activities to buttons using Butterknife.

--- app/src/main/java/com/epicodus/vancouverrangers/MainActivity.java
+++ app/src/main/java/com/epicodus/vancouverrangers/MainActivity.java
@@ -2,8 +2,15 @@ package com.epicodus.vancouverrangers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.rosterButton) Button mRosterButton;
+    @Bind(R.id.newsButton) Button mNewsButton;
+    @Bind(R.id.contactButton) Button mContactButton;
+    @Bind(R.id.scheduleButton) Button mScheduleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
