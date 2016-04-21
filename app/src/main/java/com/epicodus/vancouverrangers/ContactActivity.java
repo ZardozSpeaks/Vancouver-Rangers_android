d69a7e0d701a281a0b505717c3eeb0b0347a636c
david remington and kevin mattison <mattisonii@yahoo.com>
2016-04-21 15:47:08 -0700

    Add dial out link to phone number in ContactActivity.

--- app/src/main/java/com/epicodus/vancouverrangers/ContactActivity.java
+++ app/src/main/java/com/epicodus/vancouverrangers/ContactActivity.java
@@ -1,13 +1,33 @@
 package com.epicodus.vancouverrangers;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static java.security.AccessController.getContext;
 
 public class ContactActivity extends AppCompatActivity {
+    @Bind(R.id.phoneView) View mPhoneView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        ButterKnife.bind(this);
+
+        mPhoneView.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+                 Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "360-896-8700"));
+                 startActivity(intent);
+
+             }
+        });
     }
 }
