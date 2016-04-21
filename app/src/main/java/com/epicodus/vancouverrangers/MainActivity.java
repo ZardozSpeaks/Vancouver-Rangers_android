eb3fbcfe2ede47b65db3574b126fc5ecaff7db96
david remington and kevin mattison <mattisonii@yahoo.com>
2016-04-21 10:31:27 -0700

    Style main page buttons.

--- app/src/main/java/com/epicodus/vancouverrangers/MainActivity.java
+++ app/src/main/java/com/epicodus/vancouverrangers/MainActivity.java
@@ -1,12 +1,15 @@
 package com.epicodus.vancouverrangers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.rosterButton) Button mRosterButton;
     @Bind(R.id.newsButton) Button mNewsButton;
     @Bind(R.id.contactButton) Button mContactButton;
@@ -16,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mContactButton.setOnClickListener(this);
+        mNewsButton.setOnClickListener(this);
+        mRosterButton.setOnClickListener(this);
+        mScheduleButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mContactButton) {
+            Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+            startActivity(intent);
+        }
     }
 }
